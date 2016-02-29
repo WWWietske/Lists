812b6ff14098d38a6ab26369506c4131ebd2397c
WWWietske <wietskedotinga@hotmail.com>
2016-02-29 01:22:24 +0100

    PrintStream working and finished app

--- app/src/main/java/com/example/user/lists/MainActivity.java
+++ app/src/main/java/com/example/user/lists/MainActivity.java
@@ -9,12 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Wietske Dotinga - 10781889
  * Todolist app with one screen. Todos can be added and can later be deleted with a longclick.
- * Using ArrayAdapter to add and delete todos from the listview.
+ * Using ArrayAdapter to add and delete todos from the listview. Arraylist is saved with
+ * prinstream in a text file.
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -30,12 +35,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // On opening app check if there is a saved ArrayList of todos
-        if (savedInstanceState != null) {
-            todoArraylist = savedInstanceState.getStringArrayList("ARRAY_LIST");
-        } else {
-            todoArraylist = new ArrayList<>();
-        }
+        // Set a new ArrayList upon opening the app
+        todoArraylist = new ArrayList<>();
+
+        // Try to open the saved todos and fill the arraylist with it
+        open();
 
         // Initialize listview, edittext and button
         todoListview = (ListView) findViewById(R.id.todoListview);
@@ -49,12 +53,16 @@ public class MainActivity extends AppCompatActivity {
         // Set onclicklistener for the button
         addButton.setOnClickListener(buttonClicked);
 
+        // OnItemLongClickListener to delete item by clicking on it
         todoListview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Delete the item from the arraylist
                 todoArraylist.remove(position);
                 adapter.notifyDataSetChanged();
-                return false;
+                write();
+                return true;
             }
         });
     }
@@ -62,27 +70,46 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener buttonClicked = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
             // Add the text from the edittext into the arraylist of todos
             String todoText = todoEdit.getText().toString();
             todoArraylist.add(todoText);
             adapter.notifyDataSetChanged();
             todoEdit.setText("");
+            write();
         }
     };
 
-    // TODO tekst file storen? Ipv in oninstancestate....?
 
-    // Save todolist with onsaveinstancestate
-    @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putStringArrayList("ARRAY_LIST", todoArraylist);
+    // Open saved file if any
+    public void open(){
+
+        // Try to open the file
+        try {
+            Scanner scan = new Scanner(openFileInput("todolist.txt"));
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                todoArraylist.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    // Restore the saved todolist on restoring the app
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        todoArraylist = savedInstanceState.getStringArrayList("ARRAY_LIST");
+    // Write the arraylist items to file
+    public void write(){
+
+        // Try to create and write to the file
+        try {
+            PrintStream out = new PrintStream(openFileOutput("todolist.txt", MODE_PRIVATE));
+
+            // Write every line to the file
+            for (int i = 0; i < todoArraylist.size(); i++) {
+                out.println(todoArraylist.get(i));
+            }
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
